5e84c4fd1e2a585b5674748af37f3ef0e9c86076
atharvagadkari05 <atharvagadkari0512@gmail.com>
2021-05-24 19:13:33 +0530

    Footer updates

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,27 @@ import "../../Styles/main.css"
 const Footer = () => {
   return (
     <div className="Footer">
-      
+      <div className="container">
+        <span className="logo">
+        <a href="/">
+            Growmine<text>.org</text>
+          </a>
+        </span>
+        <div className="content">
+          <ul className ="footer options">
+            <li><a href ="/explore" >Explore</a></li>
+            <li><a href ="/contribute" >Contribute</a></li>
+            <li><a href ="/login" >Login</a></li>
+            <li><a href ="/create-account" >Create an Account</a></li>
+          </ul>
+        </div>
+        <div className="footer-links">
+          {/* Social media Icons to be added */}
+        <i class="fab fa-github"></i> 
+        </div>
+      </div>
     </div>
   )
 };
+
+export default Footer
